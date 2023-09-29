6ba1a9b2b6c6331f29547b225ba7ee663432f9c1
ukino <arfianoj@gmail.com>
2023-09-29 20:43:18 +0700

    added .env

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const dotenv = require("dotenv")
+dotenv.config()
 const mongoose = require("mongoose")
 
 const app = require("./app")
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
-const database = "mongodb://127.0.0.1:27017/tours"
+const database = process.env.DATABASE_URI
 
 mongoose
   .connect(database, {
